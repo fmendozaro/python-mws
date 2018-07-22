c3abfd40576deb7909c28de9bd3451ba56cfbba1
Fernando <fernandoza@gmail.com>
2018-07-22 16:10:34 -0500

    feat: add HTTP hello world

---
+++ HelloServer.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        # Headers
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        # Body
+        self.wfile.write('Hello, HTTP\n'.encode())
+
+
+if __name__ == '__main__':
+    server_address = ('', 8383)
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
